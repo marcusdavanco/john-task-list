a8a76c4fec8a7f7a7d513dddaa690403e99691f2
Marcus Davanço <md.daves@gmail.com>
2023-08-07 13:17:51 -0300

    chore: Todo description line clamping

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -31,7 +31,7 @@ export function Task({ completed }: TaskProps) {
               className={`appearance-none relative p-1 h-[26px] before:inline-block before:w-[18px] before:p-1 before:h-[18px] before:bg-transparent before:absolute: before:left-0 before:rounded-full before:border-2 before:border-secondary-300 before:transition before:duration-200 before:opacity-100 before:checked:opacity-0 after:inline-block after:w-[18px] after:h-[18px] after:left-[4px] after:top-[4px] after:checked:bg-secondary-300 after:absolute after:rounded-full after:border-2 after:border-none after:transition after:duration-200 after:opacity-100 after:checked:opacity-100 after:checked:bg-checkmark after:checked:bg-no-repeat after:checked:bg-center after:checked:bg-auto`}
             />
             <div className="flex flex-col gap-[10px]">
-              <span className={`font-bold ${completed ? ' text-gray-700 line-through' : 'text-gray-300'}`}>Todo</span>
+              <span className={`font-bold line-clamp-1 mr-4 ${completed ? ' text-gray-700 line-through' : 'text-gray-300'}`}>Really long todo description for testing purposes</span>
               <div className="flex gap-1 items-center"><Calendar size={14} className={`text-xs ${completed ? 'text-gray-700' : 'text-gray-400'}`} /><span className={`text-xs ${completed ? 'text-gray-700' : 'text-gray-500'}`}>Due 07/17/23</span></div>
             </div>
           </div>
