8b18bb5ec7e3eb142804f51aac4b292168b098e0
Marcus Davanço <md.daves@gmail.com>
2023-08-10 14:13:52 -0300

    wip: route handlers

--- src/app/api/subtasks/[subtask]/route.ts
+++ src/app/api/subtasks/[subtask]/route.ts
@@ -1,11 +1,38 @@
-export async function PUT() {
-  // TODO
+import { NextRequest, NextResponse } from 'next/server'
+import { UpdateUseCase } from '@/use-cases/subtasks/update'
+import { ToggleCompletedUseCase } from '@/use-cases/subtasks/toggleCompleted'
+import { DeleteUseCase } from '@/use-cases/subtasks/delete'
+import { PrismaSubtasksRepository } from '@/repositories/subtasks/prisma/prisma-subtasks-repository'
+
+export async function PUT(req: NextRequest) {
+  const subtasksRepository = new PrismaSubtasksRepository()
+  const updateUseCase = new UpdateUseCase(subtasksRepository)
+
+  const { id, title, due_date } = await req.json()
+
+  const updatedSubtask = await updateUseCase.execute({ id, title, due_date })
+
+  return NextResponse.json(updatedSubtask, {status: 200})
 }
 
-export async function PATCH() {
-  // TODO
+export async function PATCH(req: NextRequest) {
+  const subtasksRepository = new PrismaSubtasksRepository()
+  const toggleCompletedUseCase = new ToggleCompletedUseCase(subtasksRepository)
+
+  const { id } = await req.json()
+
+  const updatedSubtask = toggleCompletedUseCase.execute({ id })
+
+  return NextResponse.json(updatedSubtask, {status: 200})
 }
 
-export async function DELETE() {
-  // TODO
+export async function DELETE(req: NextRequest) {
+  const subtasksRepository = new PrismaSubtasksRepository()
+  const deleteUseCase = new DeleteUseCase(subtasksRepository)
+
+  const { id } = await req.json()
+
+  const deletedSubtask = deleteUseCase.execute({ id })
+
+  return NextResponse.json(deletedSubtask, {status: 200})
 }
--- src/app/api/tasks/[task]/route.ts
+++ src/app/api/tasks/[task]/route.ts
@@ -1,11 +1,38 @@
-export async function PUT() {
-  // TODO
+import { NextRequest, NextResponse } from 'next/server'
+import { UpdateUseCase } from '@/use-cases/tasks/update'
+import { ToggleCompletedUseCase } from '@/use-cases/tasks/toggleCompleted'
+import { DeleteUseCase } from '@/use-cases/tasks/delete'
+import { PrismaTasksRepository } from '@/repositories/tasks/prisma/prisma-tasks-repository'
+
+export async function PUT(req: NextRequest) {
+  const tasksRepository = new PrismaTasksRepository()
+  const updateUseCase = new UpdateUseCase(tasksRepository)
+
+  const { id, title, due_date } = await req.json()
+
+  const updatedTask = await updateUseCase.execute({ id, title, due_date })
+
+  return NextResponse.json(updatedTask, {status: 200})
 }
 
-export async function PATCH() {
-  // TODO
+export async function PATCH(req: NextRequest) {
+  const tasksRepository = new PrismaTasksRepository()
+  const toggleCompletedUseCase = new ToggleCompletedUseCase(tasksRepository)
+
+  const { id } = await req.json()
+
+  const updatedTask = toggleCompletedUseCase.execute({ id })
+
+  return NextResponse.json(updatedTask, {status: 200})
 }
 
-export async function DELETE() {
-  // TODO
+export async function DELETE(req: NextRequest) {
+  const tasksRepository = new PrismaTasksRepository()
+  const deleteUseCase = new DeleteUseCase(tasksRepository)
+
+  const { id } = await req.json()
+
+  const deletedTask = deleteUseCase.execute({ id })
+
+  return NextResponse.json(deletedTask, {status: 200})
 }
