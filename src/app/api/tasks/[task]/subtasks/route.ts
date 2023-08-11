f3bbb567956c7c7db3a9d9a7bb1aff9d34ac7611
Marcus Davanço <md.daves@gmail.com>
2023-08-11 10:42:09 -0300

    feat: subtask error handling

--- src/app/api/tasks/[task]/subtasks/route.ts
+++ src/app/api/tasks/[task]/subtasks/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { ListUseCase } from '@/use-cases/subtasks/list'
 import { RegisterUseCase } from '@/use-cases/subtasks/register'
 import { PrismaSubtasksRepository } from '@/repositories/subtasks/prisma/prisma-subtasks-repository'
+import { z } from 'zod'
 
 export const subtasksRepository = new PrismaSubtasksRepository()
 
@@ -18,12 +19,25 @@ export async function GET(req: NextRequest) {
 }
 
 export async function POST(req: NextRequest) {
-  const registerUseCase = new RegisterUseCase(subtasksRepository)
-
-  const { title, due_date } = await req.json()
   const task_id = req.nextUrl.pathname.split('/')[3]
+  
+  const registerBodySchema = z.object({
+    title: z.string(),
+    due_date: z.string().optional(),
+  })
 
-  const subtask = await registerUseCase.execute({ title, due_date, task_id })
+  try {
+    const { title, due_date } = registerBodySchema.parse(await req.json())
+    let dueDate = due_date ? new Date(due_date) : undefined
 
-  return NextResponse.json(subtask, { status: 201 })
+    const registerUseCase = new RegisterUseCase(subtasksRepository)
+    await registerUseCase.execute({ title, due_date: dueDate, task_id })
+
+  } catch (err) {
+    if (err instanceof z.ZodError) {      
+      return NextResponse.json({ message: err.message }, { status: 422 })
+    }   
+  }  
+
+  return NextResponse.json({ message: 'Subtask created' }, { status: 201 })
 }
