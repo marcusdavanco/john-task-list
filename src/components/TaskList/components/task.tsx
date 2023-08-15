dac30cfe6ad23fe7f2e6484dd0c5df910f5f8615
Marcus Davanço <md.daves@gmail.com>
2023-08-15 15:54:36 -0300

    feat: component animation (#14)

--- src/components/TaskList/components/task.tsx
+++ src/components/TaskList/components/task.tsx
@@ -79,56 +79,62 @@ export function Task({
   )
 
   return (
-    <Card opaque={!completed}>
-      <div className="flex w-full justify-between items-center">
-        <div className="flex gap-6 items-center">
-          <input
-            checked={completed}
-            type="checkbox"
-            onChange={handleChange}
-            className={`appearance-none relative p-1 h-[26px] before:inline-block before:w-[18px] before:p-1 before:h-[18px] before:bg-transparent before:absolute: before:left-0 before:rounded-full before:border-2 before:border-secondary-300 before:transition before:duration-200 before:opacity-100 before:checked:opacity-0 after:inline-block after:w-[18px] after:h-[18px] after:left-[4px] after:top-[4px] after:checked:bg-secondary-300 after:absolute after:rounded-full after:border-2 after:border-none after:transition after:duration-200 after:opacity-100 after:checked:opacity-100 after:checked:bg-checkmark after:checked:bg-no-repeat after:checked:bg-center after:checked:bg-auto`}
-          />
-          <div className="flex flex-col gap-[10px]">
-            <span
-              onClick={() => !isSubtask && router.push(`/tasks/${id}/subtasks`)}
-              {...attrs}
-              className={`font-bold line-clamp-1 mr-4 ${
-                completed
-                  ? ' text-gray-700 line-through'
-                  : 'text-gray-300 cursor-pointer'
-              }`}
-            >
-              {title}
-            </span>
+    <div className="flex w-full md:hover:scale-105 hover:transition hover:duration-300">
+      <Card opaque={!completed}>
+        <div className="flex w-full justify-between items-center">
+          <div className="flex gap-6 items-center">
+            <input
+              checked={completed}
+              type="checkbox"
+              onChange={handleChange}
+              className={`appearance-none relative p-1 h-[26px] before:inline-block before:w-[18px] before:p-1 before:h-[18px] before:bg-transparent before:absolute: before:left-0 before:rounded-full before:border-2 before:border-secondary-300 before:transition before:duration-200 before:opacity-100 before:checked:opacity-0 after:inline-block after:w-[18px] after:h-[18px] after:left-[4px] after:top-[4px] after:checked:bg-secondary-300 after:absolute after:rounded-full after:border-2 after:border-none after:transition after:duration-200 after:opacity-100 after:checked:opacity-100 after:checked:bg-checkmark after:checked:bg-no-repeat after:checked:bg-center after:checked:bg-auto`}
+            />
+            <div className="flex flex-col gap-[10px]">
+              <span
+                onClick={() =>
+                  !isSubtask && router.push(`/tasks/${id}/subtasks`)
+                }
+                {...attrs}
+                className={`font-bold line-clamp-1 mr-4 ${
+                  completed
+                    ? ' text-gray-700 line-through'
+                    : 'text-gray-300 cursor-pointer'
+                } md:hover:text-secondary-300 md:hover:duration-300 md:hover:transition-all`}
+              >
+                {title}
+              </span>
 
-            {due_date && (
-              <div className="flex gap-1 items-center">
-                <Calendar
-                  size={14}
-                  className={`text-xs ${
-                    completed ? 'text-gray-700' : 'text-gray-400'
-                  }`}
-                />
-                <span
-                  className={`text-xs ${
-                    completed ? 'text-gray-700' : 'text-gray-500'
-                  }`}
-                >
-                  {`Due ${due_date
-                    .split('T')[0]
-                    .replace(/(\d{4})-(\d{2})-(\d{2})/g, '$3/$2/$1')}`}
-                </span>
-              </div>
-            )}
+              {due_date && (
+                <div className="flex gap-1 items-center">
+                  <Calendar
+                    size={14}
+                    className={`text-xs ${
+                      completed ? 'text-gray-700' : 'text-gray-400'
+                    }`}
+                  />
+                  <span
+                    className={`text-xs ${
+                      completed ? 'text-gray-700' : 'text-gray-500'
+                    }`}
+                  >
+                    {`Due ${due_date
+                      .split('T')[0]
+                      .replace(/(\d{4})-(\d{2})-(\d{2})/g, '$3/$2/$1')}`}
+                  </span>
+                </div>
+              )}
+            </div>
           </div>
+          <button onClick={handleDelete}>
+            <Trash2
+              size={20}
+              className={`${
+                completed ? 'text-gray-700' : 'text-gray-400'
+              } md:hover:brightness-75 md:hover:duration-300 md:hover:transition-all`}
+            />
+          </button>
         </div>
-        <button onClick={handleDelete}>
-          <Trash2
-            size={20}
-            className={`${completed ? 'text-gray-700' : 'text-gray-400'}`}
-          />
-        </button>
-      </div>
-    </Card>
+      </Card>
+    </div>
   )
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -10,7 +10,7 @@ export function Button() {
   return (
     <button
       onClick={() => router.push(`${path}/manage`)}
-      className="flex justify-center items-center h-12 w-12 rounded-full bg-secondary-300 fixed bottom-7 lg:bottom-[5rem] right-3 lg:right-[50%]"
+      className="flex justify-center items-center h-12 w-12 rounded-full bg-secondary-300 fixed bottom-7 lg:bottom-[5rem] right-3 lg:right-[50%] brightness-75 md:hover:scale-110 md:hover:brightness-100 hover:transition hover:duration-300"
     >
       <Plus size={28} className="text-white" />
     </button>
